f49daa20bd7ce4fe90e5de988156d2803a5d1a6d
bazilbasi726 <basiljacob493@gmail.com>
2025-10-31 14:55:16 +0100

    Refine scaffold executable messaging

--- c-note-encryptor/src/main.c
+++ c-note-encryptor/src/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-
 #include "encrypt.h"
 #include "loglist.h"
 
@@ -9,10 +7,9 @@ static void print_usage(const char *program_name) {
 }
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
+    const char *program_name = (argc > 0 && argv && argv[0]) ? argv[0] : "c-note-encryptor";
 
-    print_usage("c-note-encryptor");
+    print_usage(program_name);
     puts("Scaffold executable placeholder - no functionality yet.");
 
     return 0;
